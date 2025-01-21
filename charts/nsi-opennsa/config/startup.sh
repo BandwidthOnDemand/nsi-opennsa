0247414c222b79141bbeb1208384c2eed21e48ce
Hans Trompert <hans.trompert@surf.nl>
2025-01-21 16:31:07 +0100

    enable venv in startup.sh

--- charts/nsi-opennsa/config/startup.sh
+++ charts/nsi-opennsa/config/startup.sh
@@ -2,4 +2,5 @@ export DATABASE=${PGDATABASE}
 export DATABASE_USER=${PGUSER}
 export DATABASE_PASSWORD=${PGPASSWORD}
 export DATABASE_HOST=${PGHOST}
+. /home/$USER/venv/bin/activate
 exec twistd -ny /config/opennsa.tac
